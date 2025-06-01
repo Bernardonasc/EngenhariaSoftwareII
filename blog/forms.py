c3e24872dfbc2b89a901b65d11ecbe5565d14885
bernardonunes-hotmart <bernardodonascimentonunes@gmail.com>
2025-06-01 16:16:04 -0300

    feat: styling

--- blog/forms.py
+++ blog/forms.py
@@ -2,21 +2,11 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
-    RATING_CHOICES = [(i, '⭐' * i) for i in range(1, 6)]
-
-    rating = forms.ChoiceField(
-        choices=RATING_CHOICES,
-        widget=forms.RadioSelect,
-        label="Avaliação"
-    )
-
     class Meta:
         model = Post
-        fields = ['title', 'artist', 'album', 'album_cover_url', 'rating', 'description']
-        labels = {
-            'title': 'Título da Crítica',
-            'artist': 'Artista',
-            'album': 'Álbum',
-            'album_cover_url': 'URL da Capa do Álbum (opcional)',
-            'description': 'Descrição',
+        fields = ['title', 'album', 'album_cover_url', 'artist', 'rating', 'description']
+        widgets = {
+            'album': forms.HiddenInput(),
+            'album_cover_url': forms.HiddenInput(),
+            'artist': forms.HiddenInput(),
         }
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
-    title = models.CharField("Título da Crítica", max_length=100)
-    artist = models.CharField("Artista", max_length=100)
-    album = models.CharField("Álbum", max_length=100)
-    album_cover_url = models.URLField("URL da Capa do Álbum", blank=True, null=True)
-    rating = models.IntegerField("Avaliação (1 a 5)")
-    description = models.TextField("Descrição")
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    album_cover_url = models.URLField()
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    description = RichTextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.title} – {self.artist}"
+        return f"{self.title} - {self.album}"
