612e8d80d1f8e3da806e4a2abdff81fb5cde6c42
Tomas Lacerda <tomastlm2000@gmail.com>
2025-06-02 12:40:18 -0300

    Feat: Login System Operational

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Post
 from .forms import PostForm
 
@@ -11,12 +14,27 @@ def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
             return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # faz login automático após cadastro
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
